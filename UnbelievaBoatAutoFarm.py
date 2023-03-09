6b191254b1129b371d9d917f4a9281f0fbece450
EagleDevs <58715617+Codyshep@users.noreply.github.com>
2023-03-09 04:12:29 -0600

    Add files via upload

---
+++ UnbelievaBoatAutoFarm.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+
+while True:
+    pyautogui.typewrite('/work')
+    time.sleep(0.3)
+    pyautogui.press('enter')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(0.1)
+    pyautogui.typewrite('/deposit')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(30)
